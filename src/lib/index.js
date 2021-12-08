af16ac5d422b6ec93207f0372fb711882a773c22
User <user@Users-MBP.netis>
2021-12-08 18:28:09 +0300

    added Scroller to index file

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
 export { default as Image } from './Image.svelte'
 export { default as CopyText } from './CopyText.svelte'
 export { default as Ticker } from './Ticker.svelte'
+export { default as Scroller } from './Scroller.svelte'
